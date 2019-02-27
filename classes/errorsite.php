362adf9a5a3da8263814b3109d757c53a246a537
Szymon <onufry7@gmail.com>
2019-02-27 03:08:59 +0100

    Optymalizacja kodu

--- classes/errorsite.php
+++ classes/errorsite.php
@@ -9,24 +9,12 @@ class ErrorSite extends Site
 
 	public function __construct($nr)
 	{
-		switch ($nr) 
-		{
-			case 404:
-				$this->error404();
-				$content = $this->createErr404();
-				$page = $this->getMasterView($content);
-				$view = new View;
-				$view->renderView($page);
-				break;
-			
-			default:
-				parent::__construct($this->_header, $this->_title);
-				$content = $this->createErr();
-				$page = $this->getMasterView($content);
-				$view = new View;
-				$view->renderView($page);
-				break;
-		}
+		if($nr == 404) $this->error404();
+		parent::__construct($this->_header, $this->_title);
+		$content = $this->createErr();
+		$page = $this->getMasterView($content);
+		$view = new View;
+		$view->renderView($page);
 	}
 
 
@@ -36,18 +24,6 @@ class ErrorSite extends Site
 		$this->_title = 'Błąd 404';
 		$this->_header = 'Błąd 404 - Brak Strony !';
 		$this->_message = 'Nie udało się odnaleźć strony pod tym adresem!';
-		parent::__construct($this->_header, $this->_title);
-	}
-
-
-	private function createErr404()
-	{
-		$replace = [$this->_message];
-		$search = ['{{ CONTENT }}'];
-		$view = new View;
-		$page = $view->getView('error404');
-		$content = str_replace($search, $replace, $page);
-		return $content;
 	}
 
 
